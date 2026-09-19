c1110f796a2a88cea4eeaa2015e18aa98f537dd7
grupomemoriaufabc <grupomemoriaufabc@gmail.com>
2026-09-19 09:09:20 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -6,8 +6,8 @@ import "./styles.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouterr>
   </StrictMode>
 );
